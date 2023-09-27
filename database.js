ead696f49c7beb0eb12c41865a20177f3f666b5d
falgunmahajan <falgunmahajan1999@gmail.com>
2023-09-27 17:38:04 +0530

    removed node modules

--- database.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("My Doctors", "postgres", "mydoctor", {
-    host: "localhost",
-    dialect: "postgres",
-  });
-  module.exports={sequelize}
