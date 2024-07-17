aaaa94f49a5efa6cc0bb47eb10d754bec9301d15
Falgun-Mahajan-Avisoft <falgun.mahajan@avisoft.io>
2024-07-17 11:36:40 +0530

    commit

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 
 import {Sequelize} from "sequelize"
-export const sequelize = new Sequelize("My Doctors", "postgres", "mydoctor", {
+export const sequelize = new Sequelize("My Doctors", "postgres", "falgunmahajan", {
     host: "localhost",
     dialect: "postgres",
     logging:false
